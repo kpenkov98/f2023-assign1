1189ada8a7417d75dde5de01961f695301a13a74
kpenkov98 <kpenk209@mtroyal.ca>
2023-09-30 22:24:32 -0600

    exported sql files for xammp ...unless i dont know what i am doing (big chance)

---
+++ sql/types.sql
@@ -0,0 +1,7 @@
+
+-- Table: types
+CREATE TABLE IF NOT EXISTS types (type_id INTEGER PRIMARY KEY, type_name TEXT);
+INSERT INTO types (type_id, type_name) VALUES (1, 'Band/Group');
+INSERT INTO types (type_id, type_name) VALUES (2, 'Duo');
+INSERT INTO types (type_id, type_name) VALUES (3, 'Solo');
+INSERT INTO types (type_id, type_name) VALUES (4, 'Trio');
